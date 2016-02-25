10b68bf1810138f780fbe1f59bf5da64a24a90e3
jongin <jongin66@gmail.com>
2016-02-25 20:51:56 +0900

    7777

--- g2/src/main/java/bitcamp/java77/Hello.java
+++ g2/src/main/java/bitcamp/java77/Hello.java
@@ -4,9 +4,17 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("홍길동");
+
+		System.out.println("고길동");
+		
+		for(int i=0;i<10;i++){
+		System.out.println("고길동");
+		}
+		System.out.println("성규 바보");
 		System.out.println("송창섭");
 		System.out.println("성규 바보");
 		System.out.println("그렇구만");
+
 	}
 
 }
