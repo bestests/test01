1afc732b6d8312a3e14805365c77828cfde2b4a1
bestests <albatross0210@gmail.com>
2016-02-25 20:50:46 +0900

    .

--- g2/src/main/java/bitcamp/java77/Hello.java
+++ g2/src/main/java/bitcamp/java77/Hello.java
@@ -4,8 +4,9 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("홍길동");
-		System.out.println("그렇구만");
+		System.out.println("송창섭");
 		System.out.println("성규 바보");
+		System.out.println("그렇구만");
 	}
 
 }
